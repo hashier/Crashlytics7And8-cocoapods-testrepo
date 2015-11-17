d10e5d7d2a79da6463e99dbad8e63d8091f99877
Christopher Loessl <cloessl+github@gmail.com>
2015-11-17 14:18:20 +0100

    Initial Commit

---
+++ Crashlytics7And8/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Crashlytics7And8
+//
+//  Created by Christopher Loessl on 17/11/15.
+//  Copyright © 2015 Christopher Loessl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Crashlytics7And8/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Crashlytics7And8
+//
+//  Created by Christopher Loessl on 17/11/15.
+//  Copyright © 2015 Christopher Loessl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
